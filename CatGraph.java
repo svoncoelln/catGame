47d9ffdcb302fd8a48f683c93b19c9fa9ca2a240
von Coelln <svoncoelln@friendsbalt.org>
2023-04-25 15:37:12 -0400

    remove unnecessary overrides

--- CatGraph.java
+++ CatGraph.java
@@ -1,6 +1,5 @@
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
 import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class CatGraph extends EdgeWeightedGraph {
     private Bag<CatEdge>[] adj;
@@ -12,27 +11,6 @@ public class CatGraph extends EdgeWeightedGraph {
             adj[v] = new Bag<CatEdge>();
         }
     }
-
-    public CatGraph(int V, int E) {
-        this(V);
-        if (E < 0) throw new IllegalArgumentException("Number of edges must be non-negative");
-        for (int i = 0; i < E; i++) {
-            int v = StdRandom.uniformInt(V);
-            int w = StdRandom.uniformInt(V);
-            double weight = 0.01 * StdRandom.uniformInt(0, 100);
-            CatEdge e = new CatEdge(v, w, weight);
-            addEdge(e);
-        }
-    }
-
-    public void addEdge(CatEdge e) {
-        super.addEdge(e);
-    }
-
-    public Iterable<CatEdge> adj(int v) {
-        super.adj(v);
-    }
-
 }
 
 
